bd76dd40237e36415a0f968a3f9ac608ff9b85c0
DeepakSaha <deepak.saha@africa.airtel.com>
2023-05-23 00:04:49 +0530

    home ui code moved to home package

--- app/src/main/java/com/example/newsappassignment/app/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/newsappassignment/app/ui/home/HomeActivity.kt
@@ -1,4 +1,4 @@
-package com.example.newsappassignment.app.ui
+package com.example.newsappassignment.app.ui.home
 
 import android.os.Bundle
 import android.util.Log
--- app/src/main/java/com/example/newsappassignment/app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/newsappassignment/app/ui/home/HomeViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.newsappassignment.app.ui
+package com.example.newsappassignment.app.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
