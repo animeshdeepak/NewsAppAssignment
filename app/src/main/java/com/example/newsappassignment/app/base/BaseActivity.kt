ec948d81c21f6e50179e343d40766f4c8ce2d9d2
DeepakSaha <deepak.saha@africa.airtel.com>
2023-05-29 01:02:33 +0530

    hide status bar

--- app/src/main/java/com/example/newsappassignment/app/base/BaseActivity.kt
+++ app/src/main/java/com/example/newsappassignment/app/base/BaseActivity.kt
@@ -1,8 +1,22 @@
 package com.example.newsappassignment.app.base
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 abstract class BaseActivity : AppCompatActivity() {
     abstract fun initUI()
     abstract fun initObserve()
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        hideStatusBar()
+    }
+
+    private fun hideStatusBar() {
+        WindowCompat.setDecorFitsSystemWindows(
+            window, false
+        )
+    }
 }
