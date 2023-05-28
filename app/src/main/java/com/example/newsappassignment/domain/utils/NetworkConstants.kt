ceb18d50bffc58a8bd210bd9e944470afd2271a1
DeepakSaha <deepak.saha@africa.airtel.com>
2023-05-29 01:13:46 +0530

    APP_UI_URL_REF

--- app/src/main/java/com/example/newsappassignment/domain/utils/NetworkConstants.kt
+++ app/src/main/java/com/example/newsappassignment/domain/utils/NetworkConstants.kt
@@ -4,3 +4,4 @@ const val API_KEY = ""
 const val BASE_URL = "https://newsapi.org"
 const val SEARCH_NEWS_TIME_DELAY = 500L
 const val QUERY_PAGE_SIZE = 10
+const val APP_UI_URL_REF = "https://www.behance.net/gallery/167443601/Blogging-App"
